2d083ea4dca3cfad45fa7be028624bebfdb2ec1c
Iloba <scarlettsney@gmail.com>
2021-05-02 20:27:58 +0100

    push

--- resources/views/layouts/students-nav.blade.php
+++ resources/views/layouts/students-nav.blade.php
@@ -47,7 +47,7 @@
                             @if (session()->has('LoggedUser'))
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                      A student
+                                     {{$loggedUserInfo->student_name}}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
